8629e79578c35e0072dfd0daac87fa381726653f
zingfeng <thanhdat.finance@gmail.com>
2023-05-10 17:14:42 +0700

    feat: update docs for ping pong

--- src/module/email/common.controller.ts
+++ src/module/email/common.controller.ts
@@ -1,8 +1,20 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { ResponseStatus } from '../basic/basic.dto';
+import { BasicResponse, ResponseStatus } from '../basic/basic.dto';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { PingDto } from './dto/ping.dto';
 
 @Controller('common')
 export class CommonController {
+  @ApiTags('common')
+  @ApiResponse({
+    status: 200,
+    type: BasicResponse,
+  })
+  @ApiQuery({ name: 'message', type: PingDto })
+  @ApiOperation({
+    summary:
+      'Endpoint for client to check if client config is correct and the server is alive',
+  })
   @Get('ping')
   async send(@Query('message') message: string) {
     return {
